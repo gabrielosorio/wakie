99e5718525e11e6cb9813422191539d909f06b82
Gabriel Osorio <gabriel.osorio.montelius@gmail.com>
2023-05-17 09:18:51 -0300

    Integrate LCD

--- wakie.ino
+++ wakie.ino
@@ -1,12 +1,20 @@
+#include <LiquidCrystal.h>
+
+// Display Pins
+// LCD      UNO   Nano
+// 4  (RS)  12    2 (D2)
+// 6  (E)   11    3 (D3)
+// 11 (D4)  5     4 (D4)
+// 12 (D5)  4     5 (D5)
+// 13 (D6)  3     6 (D6)
+// 14 (D7)  2     7 (D7)
+
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
+
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(13, OUTPUT);
+  lcd.begin(16, 2);
+  lcd.print("Pew!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(13, HIGH);
-  delay(1000);
-  digitalWrite(13, LOW);
-  delay(1000);
 }
